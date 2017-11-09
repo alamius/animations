a608f2a8ab39faea6f6c15d9be7924323a1ab0a0
alamius <anton.obersteiner@gmail.com>
2017-11-09 21:23:52 +0100

    Add files via upload
    Adding the functions for moving the ellipse around.

--- circle_inversion.js/sketch.js
+++ circle_inversion.js/sketch.js
@@ -7,6 +7,11 @@ var c_incr = 0.01; //resolution/speed of change in c
 var c_max = 10;
 // var length; //== sqrt(sq(A.x - cos(c) * r.x - Z.x) + sq(A.y - sin(c) * r.y - Z.y));
 var t_incr = 0.05;
+M_movement = {
+    noise: false,
+    velocity: true
+};
+var vel = {x:0, y:0};
 
 var diagram;
 var stretch;
@@ -29,9 +34,17 @@ function draw() {
     noFill();
     stroke(0);
     // M.y = -1 + c;
-    M = {
-        x: noise(c)*3-1.5,
-        y: noise(c+1)*3-1.5
+    if(M_movement.noise){
+        M = {
+            x: noise(c)*3-1.5,
+            y: noise(c+1)*3-1.5
+        }
+    }
+    if(M_movement.velocity && vel.active){
+        M = {
+            x: M.x + vel.x,
+            y: M.y + vel.y
+        }
     }
     stretch.show(); //grid lines
     strokeWeight(2);
@@ -76,16 +89,48 @@ function draw() {
     // // }
     // diagram.show();
     // if(frameCount > 1){
-    if(c > 10){
-        noLoop();
-    }
+    // if(c > 10){
+    //     noLoop();
+    // }
 }
 
 function keyPressed(){
-    if(keyCode == 32){
-        looping = !looping;
-        if(looping){      loop();
-        }else{            noLoop();
+    if(!M_movement.velocity){
+        if(keyCode == 32){
+            looping = !looping;
+            if(looping){      loop();
+            }else{            noLoop();
+            }
+        }else if(keyCode == RIGHT_ARROW){
+            M.x += 0.1;
+        }else if(keyCode == UP_ARROW){
+            M.y += 0.1;
+        }else if(keyCode == LEFT_ARROW){
+            M.x -= 0.1;
+        }else if(keyCode == DOWN_ARROW){
+            M.y -= 0.1;
+        }
+    }else{
+        if(
+            keyCode == 32 && !vel.active ||
+            [RIGHT_ARROW, UP_ARROW, LEFT_ARROW, DOWN_ARROW].includes(keyCode)
+        ){
+            vel.active = true;
+        }else if(keyCode == 32){
+            vel.active = false;
+        }else if(keyCode == 64 + 14){ // N
+            vel = {x:0, y:0};
+        }else if(keyCode == 64 + 13){ // M
+            M = {x:0, y:0};
+        }
+        if(keyCode == RIGHT_ARROW){
+            vel.x += 0.01;
+        }else if(keyCode == UP_ARROW){
+            vel.y += 0.01;
+        }else if(keyCode == LEFT_ARROW){
+            vel.x -= 0.01;
+        }else if(keyCode == DOWN_ARROW){
+            vel.y -= 0.01;
         }
     }
 }
